51254d947975e6b95d399e44dfcf6e4a142bcc1b
Evert Vanderstadt <evertvanderstadt@gmail.com>
2020-11-15 11:36:41 +0100

    first commit

---
+++ js/scrolling-nav.js
@@ -0,0 +1,1 @@
+$(window).scroll(function(){if($(".navbar").offset().top>50){$(".fixed-top").addClass("top-nav-collapse")}else{$(".fixed-top").removeClass("top-nav-collapse")}});$(function(){$(document).on('click','a.page-scroll',function(event){var $anchor=$(this);$('html, body').stop().animate({scrollTop:$($anchor.attr('href')).offset().top},1500,'easeInOutExpo');event.preventDefault()})})
